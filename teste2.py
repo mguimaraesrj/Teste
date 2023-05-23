05f8b7d7dc4e4046afa6e0d7fabf83476ce073f0
mguimaraesrj <133999934+mguimaraesrj@users.noreply.github.com>
2023-05-23 08:23:44 -0300

    Update teste2.py

--- teste2.py
+++ teste2.py
@@ -11,4 +11,11 @@ dataframe = pd.DataFrame({
     'Salário': [10, 20, 30, 40]
 })
 dataframe.style.highlight_max(axis=0)
+
 st.write(dataframe)
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3)
+     columns = ['a', 'b', 'c']}
+    
+st.line_chart(chart_data)
