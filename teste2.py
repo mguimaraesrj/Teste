5a53a8600df7d6b617d37656200931c4448a518b
mguimaraesrj <133999934+mguimaraesrj@users.noreply.github.com>
2023-05-23 08:24:49 -0300

    Update teste2.py

--- teste2.py
+++ teste2.py
@@ -15,7 +15,7 @@ dataframe.style.highlight_max(axis=0)
 st.write(dataframe)
 
 chart_data = pd.DataFrame(
-     np.random.randn(20, 3)
+     np.random.randn(20, 3),
      columns = ['a', 'b', 'c']}
     
 st.line_chart(chart_data)
