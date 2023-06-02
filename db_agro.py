2b7a47b20a2b17bcac3e8fbb51db3e5a76b949f3
mguimaraesrj <133999934+mguimaraesrj@users.noreply.github.com>
2023-06-02 17:50:40 -0300

    db_agro.py

---
+++ db_agro.py
@@ -0,0 +1,64 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+def obter_informacoes_commodity(commodity):
+    url = f"https://www.noticiasagricolas.com.br/cotacoes/{commodity}"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Obter o título
+    titulo = soup.find('a', {'href': f'/cotacoes/{commodity}'}).text
+    print("Título:", titulo)
+
+    # Obter as informações na segunda coluna (td) da segunda linha (tr)
+    linha_tabela = soup.find_all('tr')[1]
+    dados_tabela = linha_tabela.find_all('td')[1].text
+    print("Informação:", dados_tabela)
+
+    # Para obter o histórico de preços do produto
+    link_historico = soup.find('a', {"class": "mostrar-historico"})["href"]
+    print("Link do histórico:", link_historico)
+
+    # Acessar o novo link gerado por "link_historico"
+    novo_link = f"https://www.noticiasagricolas.com.br{link_historico}"
+    novo_response = requests.get(novo_link)
+    novo_soup = BeautifulSoup(novo_response.text, 'html.parser')
+
+    # Encontrar todas as informações com a tag <td> na nova página
+    resultados = novo_soup.find_all('td')
+
+    # Organizar os resultados em listas separadas para cada coluna
+    tipo_resultado = 0
+    datas = []
+    precos = []
+
+    for resultado in resultados:
+        texto = resultado.text.strip()
+
+        if tipo_resultado == 0:
+            datas.append(texto)
+        elif tipo_resultado == 1:
+            precos.append(texto)
+
+        tipo_resultado += 1
+        if tipo_resultado == 3:
+            tipo_resultado = 0
+
+        if len(datas) >= 11:
+            break
+
+    # Verificar se as listas têm o mesmo tamanho
+    tamanho = min(len(datas), len(precos))
+
+    # Criar um dataframe com as colunas "Datas" e "Preços"
+    df = pd.DataFrame({"Datas": datas[:tamanho], "Preços": precos[:tamanho]})
+
+    # Exibir o dataframe
+    print(df)
+
+# Obter a commodity desejada do usuário
+commodity = input("Digite o nome da commodity: ").lower().replace(' ', '-')
+
+# Chamar a função com a commodity fornecida pelo usuário
+obter_informacoes_commodity(commodity)
