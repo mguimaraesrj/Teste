ea06610b370e6820fd2b03dfbf617c4892694c5a
mguimaraesrj <133999934+mguimaraesrj@users.noreply.github.com>
2023-06-07 15:10:20 -0300

    exercicio_grafico_streamlit.py

--- exercicio_grafico_streamlit.py
+++ exercicio_grafico_streamlit.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import streamlit as st
 
 # Ler o arquivo CSV
-data = pd.read_csv('caminho/para/o/arquivo.csv')
+data = pd.read_csv('/content/age_gender.csv')
 
 # Exibir o gráfico no Streamlit
 st.title('Gráfico de Pizza a partir de um arquivo CSV')
