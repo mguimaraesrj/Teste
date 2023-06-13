f12464d72e2220122bed4d582855ab286822e043
mguimaraesrj <133999934+mguimaraesrj@users.noreply.github.com>
2023-06-13 08:24:40 -0300

    db_agro.py

--- db_agro.py
+++ db_agro.py
@@ -77,7 +77,8 @@ commodity_opcoes = ["Boi Gordo", "Soja", "Café", "Trigo", "Frango", "Laranja", 
 
 # Selecionar a commodity desejada do usuário
 commodity_atualizada = ['boi-gordo', 'soja', 'cafe', 'trigo', 'frango', 'laranja', 'milho']
-commodity = st.selectbox("Selecione uma commodity", commodity_atualizada)
+commodity = st.selectbox("Selecione uma commodity", commodity_opcoes)
+commodity = commodity_opcoes.replace("commodity_opcoes", "commodity_atualizada")
 
 
 # Verificar se alguma commodity foi selecionada antes de chamar a função
