c52a98b3aa8ed5246c2f0980ce558f2385f5f68c
mguimaraesrj <133999934+mguimaraesrj@users.noreply.github.com>
2023-06-15 19:49:20 -0300

    db_agro.py

--- db_agro.py
+++ db_agro.py
@@ -16,8 +16,9 @@ def obter_informacoes_commodity(commodity):
     # Obter as informações na segunda coluna (td) da segunda linha (tr)
     linha_tabela = soup.find_all('tr')[1]
     dados_tabela = linha_tabela.find_all('td')[1].text
-    st.sidebar.markdown("**Cotação atual**")
-    st.sidebar.write(dados_tabela)
+
+    # Exibir a cotação atual
+    st.sidebar.markdown(f"**Cotação atual:** {dados_tabela}")
 
     # Para obter o histórico de preços do produto
     link_historico = soup.find('a', {"class": "mostrar-historico"})["href"]
