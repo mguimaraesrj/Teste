a7cafc812539e415b533122d33001dcfa357651a
mguimaraesrj <133999934+mguimaraesrj@users.noreply.github.com>
2023-06-15 21:31:24 -0300

    Update db_agro.py

--- db_agro.py
+++ db_agro.py
@@ -103,7 +103,7 @@ title_html = """
 
 # Cabeçalho do aplicativo
 st.markdown(title_html, unsafe_allow_html=True)
-st.markdown('<div class="title"><h1>Agroboard 🐂 🌱 - Dashboard Agro</h1></div>', unsafe_allow_html=True)
+st.markdown('<div class="title"><h1>Agroboard - Dashboard Agro</h1></div>', unsafe_allow_html=True)
 
 # Dicionário de correlação entre a chave (opção selecionada) e o valor (commodity correspondente)
 commodity_correlacao = {
